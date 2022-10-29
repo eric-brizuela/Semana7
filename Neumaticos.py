e17776c36ae89ab02fcfbaa36ebc441fc72c8036
cabrera0110 <da_cabrera0110@live.com>
2022-10-29 02:45:13 -0600

    version 2.0

--- Neumaticos.py
+++ Neumaticos.py
@@ -1,92 +1,48 @@
-#declaracion de variables
-
-sinteticos = ""
-hibridos = ""
-naturales = ""
-precio = 0
-cantidad = 0
-descuento = 0
-subtotal = 0
-iva = 0
-monto_final = 0
-
-#definicion de funciones
-
-def sinteticos():
-  precio = 12000
-  if cantidad < 10:
-    descuento = 0
-    subtotal = cantidad * precio
-    iva = subtotal * 0.13
-    monto_final = subtotal + iva
-  elif cantidad > 10:
-    descuento = 0.95
+def neumaticos(tipo, cantidad, precio, descuento):
     subtotal = (cantidad * precio) * descuento
     iva = subtotal * 0.13
     monto_final = subtotal + iva
 
-#factura
-  print("\nGracias por su compra. A continuación, su factura:\nTipo.............. Sintéticos\nCantidad..........",cantidad,"\nSubtotal..........",subtotal,"\nDescuento.........",descuento,"\nIVA...............",iva,"\nMonto por Pagar...",monto_final)
-    
-def hibridos():
-  precio = 25000
-  if cantidad < 10:
-    descuento = 0
-    subtotal = cantidad * precio
-    iva = subtotal * 0.13
-    monto_final = subtotal + iva
-  elif cantidad > 10:
-    descuento = 0.90
-    subtotal = (cantidad * precio) * descuento
-    iva = subtotal * 0.13
-    monto_final = subtotal + iva
+    print("\nGracias por su compra. A continuación, su factura:\nTipo..............", tipo, "\nCantidad..........",
+          cantidad, "\nSubtotal..........", subtotal, "\nDescuento.........", descuento, "\nIVA...............",
+          iva, "\nMonto por Pagar...", monto_final)
 
-#factura
-  print("\nGracias por su compra. A continuación, su factura:\nTipo.............. Híbridos\nCantidad..........",cantidad,"\nSubtotal..........",subtotal,"\nDescuento.........",descuento,"\nIVA...............",iva,"\nMonto por Pagar...",monto_final)
-
-def naturales():
-  precio = 45000
-  if cantidad < 8:
-    descuento = 0
-    subtotal = cantidad * precio
-    iva = subtotal * 0.13
-    monto_final = subtotal + iva
-  elif cantidad > 8:
-    descuento = 0.93
-    subtotal = (cantidad * precio) * descuento
-    iva = subtotal * 0.13
-    monto_final = subtotal + iva
-
-#factura
-  print("\nGracias por su compra. A continuación, su factura:\nTipo.............. Naturales\nCantidad..........",cantidad,"\nSubtotal..........",subtotal,"\nDescuento.........",descuento,"\nIVA...............",iva,"\nMonto por Pagar...",monto_final)
-
-#programa
 
 while True:
-  menu = input("\nGracias por utilizar nuestro programa. A continuación, el menú de opciones: \n\n1. Comprar neumáticos. \n2. Salir. \n\nDigite el número de la opción que desea ejecutar: \n")
+    menu = input(
+        "\nGracias por utilizar nuestro programa. A continuación, el menú de opciones: \n\n1. Comprar neumáticos. "
+        "\n2. Salir. \n\nDigite el número de la opción que desea ejecutar: \n")
 
-  if menu == "1":
+    if menu == "1":
 
-    tipo = input("\nDe acuerdo. A continuación podrá seleccionar el tipo de neumáticos que desea comprar:\n\n1. Digite '1' para neumáticos sintéticos.\n2. Digite '2' para neumaticos híbridos.\n3. Digite '3' para neumáticos naturales.\n")
-    
-    cantidad = int(input("\nDigite la cantidad de neumáticos que desea comprar: \n"))
+        tipo = input(
+            "\nDe acuerdo. A continuación podrá seleccionar el tipo de neumáticos que desea comprar:\n"
+            "\n1. Digite '1' para neumáticos sintéticos.\n2. Digite '2' para neumaticos híbridos."
+            "\n3. Digite '3' para neumáticos naturales.\n")
 
-    if tipo == "1":
-      sinteticos()
+        cantidad = int(input("\nDigite la cantidad de neumáticos que desea comprar: \n"))
 
-    elif tipo == "2":
-      hibridos()
+        if tipo == "1":
+            if cantidad > 10:
+                descuento = 0.95
+            else:
+                descuento = 1
+            neumaticos("Sintéticos", cantidad, 12000, descuento)
+            print()
 
-    elif tipo == "3":
-      naturales()  
+        elif tipo == "2":
+            if cantidad > 10:
+                descuento = 0.90
+            else:
+                descuento = 1
+            neumaticos("Híbridos", cantidad, 25000, descuento)
 
-  else:
-    break
-  
+        elif tipo == "3":
+            if cantidad > 8:
+                descuento = 0.93
+            else:
+                descuento = 1
+            neumaticos("Naturales", cantidad, 45000, descuento)
 
-
-
-  
-
-
-  
+    else:
+        break
